021b15196aa9f1f1c0febb46e3c61051257faf05
valderfarkill <bigzam.3@outlook.com>
2023-09-22 14:15:14 +0200

    add immobili

--- Immobili/app.py
+++ Immobili/app.py
@@ -1,9 +1,15 @@
-import streamlit as st
 import numpy as np
 import pandas as pd
-import joblib
-import io
+import seaborn as sns
+import streamlit as st
+import plotly.express as px
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+from sklearn.model_selection import train_test_split
 import os
+import io
+import joblib
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -13,7 +19,7 @@ def add_bg_from_url():
          f"""
          <style>
          .stApp {{
-             background-image: url("https://images7.alphacoders.com/103/1038307.jpg");
+             background-image: url("https://media.makeameme.org/created/you-are-overfitting.jpg");
              background-attachment: fixed;
              background-size: cover
          }}
@@ -22,29 +28,30 @@ def add_bg_from_url():
          unsafe_allow_html=True
      )
 
-
-
-def main():
-    st.text("Iris Classification")
+def main(): 
+    st.text("Immobili")
 
     absolute_path = os.path.dirname(__file__)
-    relative_path = "regression_iris.pkl"
+    relative_path = "reg_test.pkl"
     full_path = os.path.join(absolute_path, relative_path)
+    
+    newmodel = joblib.load(full_path)
 
     file = st.file_uploader("Carica un file CSV o Excel", type=["csv", "xlsx"])
-    
-    if file is not None:
-        df = pd.read_csv(file) if file.type == "application/vnd.ms-excel" else pd.read_excel(file)
 
-        # Mostra i dati caricati
-        st.write("Dati caricati:")
-        st.write(df)
-    
-        #predictions = newmodel.predict(df[['R&D Spend', 'Administration', 'Marketing Spend']])
-        #df['Predicted Profit'] = np.round(predictions, 1)
-        #st.write(df)
-    
-        #download button
+    if file is not None:
+
+
+        if os.path.splitext(file.name)[1] ==".xlsx":
+            df = pd.read_excel(file, engine="openpyxl")
+        else:
+            df = pd.read_csv(file)
+        
+
+        y_pred = newmodel.predict(df)
+        df["price"]=y_pred
+        st.dataframe(df)
+
         output = io.BytesIO()
         writer = pd.ExcelWriter(output, engine='xlsxwriter')
         df.to_excel(writer,sheet_name='Elenco_tot', index=False)
@@ -53,22 +60,36 @@ def main():
         st.download_button(
         label="Scarica file Excel",
         data=output,
-        file_name='regr_iris.xlsx',
+        file_name='reg_test.xlsx',
         mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-    )
-    newmodel = joblib.load(full_path)
-
-    sepal_lenght = st.number_input("sepal length", 1.0,10.0,3.0)
-    sepal_width = st.number_input("sepal width", 1.0,10.0,3.0)
-    petal_lenght = st.number_input("petal length", 1.0,10.0,3.0)
-    petal_width = st.number_input("petal width", 1.0,10.0,3.0)
-    prediction = newmodel.predict([[sepal_lenght,sepal_width, petal_lenght, petal_width]])[0]
-    st.write(f"Classification iris: {prediction}")
+        )
+        
     
+    if file is None:
 
-    
-if __name__ == "__main__":
+        #inference
+        st.text("Try the model")
+        input1 = st.number_input("Insert crim value:",)
+        input2 = st.number_input("Insert zn value:",)
+        input3 = st.number_input("Insert indus value:",)
+        input4 = st.number_input("Insert chas value:",)
+        input5 = st.number_input("Insert nox value:",)
+        input6 = st.number_input("Insert rm value:",)
+        input7 = st.number_input("Insert age value:",)
+        input8 = st.number_input("Insert dis value:",)
+        input9 = st.number_input("Insert rad value:",)
+        input10 = st.number_input("Insert tax value:",)
+        input11 = st.number_input("Insert ptratio value:",)
+        input12 = st.number_input("Insert b value:",)
+        input13 = st.number_input("Insert istat value:",)
+        prediction = newmodel.predict([[input1, input2, input3, input4, input5, input6, input7, input8, input9, input10, input11, input12, input13]])
+        prediction = prediction[0]
+        st.write(f"Predicted: {round(prediction,1)}$")
+
+
+
+if __name__ == '__main__':
     add_bg_from_url() 
     main()
-
-#streamlit run app.py
+    
+# streamlit run app.py        
