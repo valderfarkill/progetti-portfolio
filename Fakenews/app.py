14acee6bf00ef3223db334931c29447fce5401c6
valderfarkill <bigzam.3@outlook.com>
2023-09-25 01:39:11 +0200

    fake news update

--- Fakenews/app.py
+++ Fakenews/app.py
@@ -2,28 +2,40 @@ import streamlit as st
 import joblib
 import re
 import os
-import nltk
-from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import TfidfVectorizer
-from scipy.sparse import csr_matrix  # Import the csr_matrix class
+import joblib
 
 
 # Streamlit app
 def main():
-    st.title('Fake News Detection App')
+    st.title("Data Mining")
+    
+    word = st.text_input("Enter the text:  ")
 
     absolute_path = os.path.dirname(__file__)
     relative_path = "real_fake.pkl"
     full_path = os.path.join(absolute_path, relative_path)
+
+    pipe = joblib.load(full_path)
     
-    newmodel = joblib.load(full_path)
+    if word == "":
+        st.warning("Input text is required.")
+    else:
+    
+        text_fake = []
+        text_fake.append(word)
+        
+        predictions = pipe.predict(text_fake)
+        
+        
+        # Cambia il colore del background in base al sentiment
+        
+        if predictions[0] == 1:
+                result = "Real News"
+        else:
+                result = "Fake News"
 
-    # Input text from user
-    input1 = st.text_area('Enter the news text:', '')
-
-    prediction = newmodel.predict([[input1]])
-    prediction = prediction[0]
-    st.write(f"Predicted: {prediction}")
-
+        st.subheader('Prediction Result:')
+        st.write(result)
+        
 if __name__ == '__main__':
     main()
